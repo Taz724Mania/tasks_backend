00b24f1668457f5e7f70dad0ff620b442c69cd4e
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-14 22:49:19 -0400

    veiws.py completed

--- task/views.py
+++ task/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Tasks
+from rest_framework import viewsets, permissions
+from .serializers import TasksSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset=Tasks.objects.all()
+    serializer_class=TasksSerializer
+    permission_classes=[permissions.AllowAny]
