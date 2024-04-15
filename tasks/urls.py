2cf3cb3ba184d74bf95923d2d721458db20b838a
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-14 22:37:45 -0400

    urls.py completed

--- tasks/urls.py
+++ tasks/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from task.views import TaskViewSet
+
+router = routers.DefaultRouter()
+router.register(r'task', TaskViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
